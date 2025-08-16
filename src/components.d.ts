a87b9af034eb78aad1bf8d56cead5801abfc003d
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-08-16 01:50:21 -0300

    envio sem erros

---
+++ src/components.d.ts
@@ -0,0 +1,16 @@
+import Navbar from "../../components/Navbar";
+import Sobre from "../../components/Sobre";
+import Card from "../../components/Card";
+import Projetos from "../../components/Projetos";
+import Footer from "../../components/Footer";
+
+// Declaração de arquivos de imagem para que TypeScript aceite imports
+declare module "*.png";
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.gif";
+declare module "*.svg";
+
+// Declaração de arquivos CSS/SCSS para que TS aceite imports
+declare module "*.css";
+declare module "*.scss";
