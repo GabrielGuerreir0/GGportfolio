2bca42ae37ed32c5acb9f8e365fdd4e21e215b57
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-08-22 01:23:27 -0300

    correcao

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,8 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
 import Contato from "../pages/Contato/Contato";
-/* import NotFound from "../pages/NotFound";
- */
+
 export default function Rotas() {
   return (
     <Routes>
