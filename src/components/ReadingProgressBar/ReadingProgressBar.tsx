617ed6a9ab955461f7161fc8d7558d2f6480d25f
Gabriel Guerreiro <136819754+GabrielGuerreir0@users.noreply.github.com>
2025-10-04 11:55:11 -0300

    concerto

--- src/components/ReadingProgressBar/ReadingProgressBar.tsx
+++ src/components/ReadingProgressBar/ReadingProgressBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 const ReadingProgressBar = () => {
   const [progress, setProgress] = useState(0);
